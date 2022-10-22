a0871ca9f001e854f22fcb9e6f27e3ce68dd0c8b
Alkida Vaci <alkida.vaci@gmail.com>
2022-10-22 18:44:01 -0400

    Check Answer and go to next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ var quiz = document.querySelector("#quiz");
 var question = document.querySelector(".question");
 
 var choicesElem = document.querySelectorAll(".choices");
-var answer1li = document.querySelector("#answer_btn1");
-var answer2li = document.querySelector("#answer_btn2");
-var answer3li = document.querySelector("#answer_btn3");
-var answer4li = document.querySelector("#answer_btn4");
+var answer1li = document.querySelector("#answer_li1");
+var answer2li = document.querySelector("#answer_li2");
+var answer3li = document.querySelector("#answer_li3");
+var answer4li = document.querySelector("#answer_li4");
 
 var correctIncorrect = document.querySelector("#correct-incorrect");
 
@@ -32,74 +32,131 @@ var questions = [
   {
     question: "What is the capital of Albania?",
     choices: ["1. Lezhe", "2. Milot", "3. Vlore", "4. Tirane"],
-    asnwer: 4,
+    answer: 4,
   },
   {
     question: "What is the national animal of Albania?",
     choices: ["1. Tiger", "2. Goat", "3. Golden Eagle", "4. Lion"],
-    asnwer: 3,
+    answer: 3,
   },
   {
     question: "How many world heritage sites are there in Albania?",
     choices: ["1. 5", "2. 10", "3. 4", "4. 2"],
-    asnwer: 3,
+    answer: 3,
   },
   {
     question: "Which of the singers are from albania?",
     choices: ["1. Bebe Rexha", "2. Ariana Grande", "3. Lady Gaga", "4. Demi Lavato"],
-    asnwer: 1,
+    answer: 1,
   },
   {
     question: "What currency is used in Albania?",
     choices: ["1. Euro", "2. Albanian Crowns", "3. Albanian Dinar", "4. Albanian Lek"],
-    asnwer: 4,
+    answer: 4,
   },
 ];
 
-var secondsLeft = 100;
+var secondsLeft = 60;
 var questionNr = 0;
 var totalScore = 0;
 var questionCount = 1;
 
 function gameOver() {
-   // Show submit results 
-   quiz.classList.add("hidden");
-   submitResult.classList.remove("hidden");
+  // Show submit results 
+  quiz.classList.add("hidden");
+  submitResult.classList.remove("hidden");
 
   // totalScore is variable for the user score in the quiz
-  userScore.textContent = "Your final score is : " + totalScore ;
- };
+  userScore.textContent = "Your final score is : " + totalScore;
 
+};
+
+// Timer function
 function setTimer() {
-        
+
   var timerInterval = setInterval(function () {
 
     secondsLeft--;
-    timeLeft.textContent =  secondsLeft + " s";
-      // No time left
-      if (secondsLeft === 0){
-          clearInterval(timerInterval);         
-          // function for ending game
-          gameOver();       
-          } 
-      // No question left
-      else  if(questionCount >= questionSource.length +1) {
-        clearInterval(timerInterval);
-        gameOver();
-        } 
-}, 1000);
+    timeLeft.textContent = secondsLeft + " s";
+    // No time left
+    if (secondsLeft <= 0) {
+     
+      clearInterval(timerInterval);
+      // function for ending game
+      gameOver();
+    }
+    // No question left
+    else if (questionCount >= questions.length + 1) {
+      timeLeft.innerHTML = "0"
+      clearInterval(timerInterval);
+      gameOver();
+    }
+
+  }, 1000);
 }
 
 // Function to start Quiz
-function startQuiz () {
+function startQuiz() {
   home.classList.add("hidden");
   quiz.classList.remove("hidden");
 
   // function to set timer
   setTimer()
   // function to show questions
+  showQuestion(questionNr)
+}
+// Declare function to show questions
 
+function showQuestion(n) {
+  question.textContent = questions[n].question;
+  answer1li.textContent = questions[n].choices[0];
+  answer2li.textContent = questions[n].choices[1];
+  answer3li.textContent = questions[n].choices[2];
+  answer4li.textContent = questions[n].choices[3];
+  questionNr = n;
 }
 
+// Declare function to check answer and go to next answer
+function checkAnswer(e) {
+  e.preventDefault();
+  //make it display
+  correctIncorrect.style.display = "block";
+  setTimeout(function () {
+    correctIncorrect.style.display = 'none';
+  }, 1000);
+
+  // Check answer is correct or Wrong
+  if (questions[questionNr].answer == e.target.value) {
+    correctIncorrect.textContent = "Correct!";
+    // store time 
+    totalScore = secondsLeft;
+
+  } else {
+    // Decrease 10 sec when is wrong and stor time
+    secondsLeft = secondsLeft - 10;
+    totalScore = secondsLeft;
+
+    correctIncorrect.textContent = "Wrong!";
+  }
+  //THEN I am presented with another question
+  if (questionNr < questions.length - 1) {
+    // call showQuestions to bring in next question when any reactBtn is clicked
+    showQuestion(questionNr + 1);
+  }
+  else {
+    gameOver();
+  }
+  questionCount++;
+}
+
+
+
+// Event Listener to for each choice
+
+choicesElem.forEach(function (e) {
+
+  // Click on choices to show if is correct answer and to go to next question 
+  e.addEventListener("click", checkAnswer);
+});
 // Event Listener to start quiz
 startBtn.addEventListener("click", startQuiz);
